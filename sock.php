077756f5fdd89806d506124b6c6668020442596c
Erin <erin.macd@outlook.com>
2017-03-09 11:03:05 +0000

    I know it's messy but this is stubbornly broken.

--- sock.php
+++ sock.php
@@ -3,6 +3,7 @@
 include_once __DIR__.'/config.php';
 include __DIR__.'/vendor/autoload.php';
 use WebSocket\Client;
+global $nowplaying;
 
 $sock_client = new Client("wss://gateway.discord.gg/?encoding=json&v=6");
 
@@ -29,12 +30,10 @@ var_dump($sock_re); echo '<br><br>';
 if(isset($nowplaying)){
   $sock_client->receive();
   $sock_client->receive();
-  $sock_client->receive();
-
   $sock_client->send($sock_push_game);
-          
   $sock_re = json_decode($sock_client->receive(), true);
   var_dump($sock_re); echo '<br><br>';
+  $sock_client->send($sock_push_game);
 }
 
 ?>       
